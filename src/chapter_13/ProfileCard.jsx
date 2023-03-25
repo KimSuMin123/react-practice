185f267a4cb135bd3cb41a6c4685cb0e614577a4
Kimsumin123 <tntn211@naver.com>
2023-03-25 12:30:07 +0900

    chapter_13 실습 업로드

---
+++ src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function  ProfileCard (props) {
+    return(
+        <Card title = "Inje Lee" backgroundColor = "#4ea04e">
+            <p>안녕하세요. 소플입니다.</p>
+            <p>저는 리엑트를 사용해서 개발 하고 있어요.</p>
+        </Card>
+    )
+}
+
+export default ProfileCard;
